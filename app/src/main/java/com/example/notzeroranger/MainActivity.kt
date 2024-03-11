dc388e7bf7394db9383df500731287b982d335e9
WindBow27 <59176912+WindBow27@users.noreply.github.com>
2024-03-11 22:54:39 +0700

    init

---
+++ app/src/main/java/com/example/notzeroranger/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.notzeroranger
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // move to settings activity
+        val settingsButton: Button = findViewById(R.id.settingsButton)
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        // quit game
+        val quitButton: Button = findViewById(R.id.quitButton)
+        quitButton.setOnClickListener {
+            finish()
+            System.exit(0)
+        }
+    }
+}
